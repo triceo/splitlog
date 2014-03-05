19e6641ce4cf0f6d9f22af9b05f79bcbc21b485b
Lukáš Petrovický <lpetrovi@redhat.com>
2014-03-05 18:55:02 -0500

    Separate the message storage into a different class.

--- splitlog-core/src/main/java/com/github/triceo/splitlog/DefaultLogWatch.java
+++ splitlog-core/src/main/java/com/github/triceo/splitlog/DefaultLogWatch.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -28,13 +27,15 @@ final class DefaultLogWatch implements LogWatch {
     private final Set<AbstractLogTailer> tailers = new LinkedHashSet<AbstractLogTailer>();
     private final LogWatchTailerListener listener;
     /*
-     * these hashmaps are weak; when a tailer is terminated and removed from
-     * logwatch, we don't want to keep it anymore
+     * These maps are weak; when a tailer stops being used by user code, we do
+     * not want these IDs to prevent it from being GC'd. Yet, for as long as the
+     * tailer is being used, we want to keep the IDs since the tailer may still
+     * ask for the messages.
      */
     private final Map<LogTailer, Integer> startingMessageIds = new WeakHashMap<LogTailer, Integer>(),
             endingMessageIds = new WeakHashMap<LogTailer, Integer>();
 
-    private final List<Message> messageQueue = new LinkedList<Message>();
+    private final MessageStore messages = new MessageStore();
 
     protected DefaultLogWatch(final File watchedFile, final TailSplitter splitter, final long delayBetweenReads,
             final boolean ignoreExistingContent, final boolean reopenBetweenReads, final int bufferSize) {
@@ -47,7 +48,7 @@ final class DefaultLogWatch implements LogWatch {
     protected synchronized void addLine(final String line) {
         final Message message = this.splitter.addLine(line);
         if (message != null) {
-            this.messageQueue.add(message);
+            this.messages.add(message);
         }
         for (final AbstractLogTailer t : this.tailers) {
             t.notifyOfLine(line);
@@ -68,7 +69,11 @@ final class DefaultLogWatch implements LogWatch {
     protected List<Message> getAllMessages(final LogTailer tail) {
         final int start = this.startingMessageIds.get(tail);
         final int end = this.getEndingId(tail);
-        return Collections.unmodifiableList(this.messageQueue.subList(start, end));
+        if (start > end) { // just in case
+            return Collections.unmodifiableList(Collections.<Message> emptyList());
+        } else {
+            return this.messages.getFromRange(start, end + 1);
+        }
     }
 
     /**
@@ -78,7 +83,8 @@ final class DefaultLogWatch implements LogWatch {
      *            Tailer in question.
      */
     private int getEndingId(final LogTailer tail) {
-        return this.endingMessageIds.containsKey(tail) ? this.endingMessageIds.get(tail) : this.messageQueue.size();
+        return this.endingMessageIds.containsKey(tail) ? this.endingMessageIds.get(tail) : this.messages
+                .getLatestMessageId();
     }
 
     @Override
@@ -93,7 +99,7 @@ final class DefaultLogWatch implements LogWatch {
 
     @Override
     public synchronized LogTailer startTailing() {
-        final int startingMessageId = this.messageQueue.size();
+        final int startingMessageId = this.messages.getNextMessageId();
         final AbstractLogTailer tail = new NonStoringLogTailer(this);
         this.tailers.add(tail);
         this.startingMessageIds.put(tail, startingMessageId);
@@ -121,7 +127,7 @@ final class DefaultLogWatch implements LogWatch {
     @Override
     public synchronized boolean terminateTailing(final LogTailer tail) {
         if (this.tailers.remove(tail)) {
-            final int endingMessageId = this.messageQueue.size();
+            final int endingMessageId = this.messages.getLatestMessageId();
             this.endingMessageIds.put(tail, endingMessageId);
             return true;
         } else {
