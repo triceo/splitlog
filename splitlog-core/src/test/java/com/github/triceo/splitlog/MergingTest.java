b980d0727f55ae8a22cbf21c7f062a86b12431ca
Lukáš Petrovický <lpetrovi@redhat.com>
2014-04-22 13:25:13 -0400

    #40: final bits of new tests for merging

--- splitlog-core/src/test/java/com/github/triceo/splitlog/MergingTest.java
+++ splitlog-core/src/test/java/com/github/triceo/splitlog/MergingTest.java
@@ -1,17 +1,87 @@
 package com.github.triceo.splitlog;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import com.github.triceo.splitlog.api.Follower;
+import com.github.triceo.splitlog.api.LogWatch;
 import com.github.triceo.splitlog.api.MergingFollower;
+import com.github.triceo.splitlog.formatters.NoopMessageFormatter;
 
 public class MergingTest extends DefaultFollowerBaseTest {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testMergeWithSelf() {
-        final Follower f = this.getLogWatch().startFollowing();
-        f.mergeWith(f);
+    private static final String MESSAGE_1 = "Message";
+    private static final String MESSAGE_2 = "Second Message";
+    private static final String MESSAGE_3 = "Third Message";
+    private static final String MESSAGE_4 = "Fourth Message";
+    private static final String MESSAGE_5 = "Fifth Message";
+
+    @Test
+    public void testMergeWithDifferentWatches() {
+        // write into first file
+        final LogWatch watch1 = LogWatchBuilder.forFile(DefaultFollowerBaseTest.getTempFile()).build();
+        final Follower follower1 = watch1.startFollowing();
+        final LogWriter writer1 = LogWriter.forFile(watch1.getWatchedFile());
+        writer1.write(MergingTest.MESSAGE_1, follower1);
+        // write into second file
+        final LogWatch watch2 = LogWatchBuilder.forFile(DefaultFollowerBaseTest.getTempFile()).build();
+        final Follower follower2 = watch2.startFollowing();
+        final LogWriter writer2 = LogWriter.forFile(watch2.getWatchedFile());
+        writer2.write(MergingTest.MESSAGE_2, follower2);
+        // merge both
+        final MergingFollower merge = follower2.mergeWith(follower1);
+        Assertions.assertThat(merge.getMerged()).containsOnly(follower1, follower2);
+        Assertions.assertThat(merge.getMessages()).hasSize(0); // zero ACCEPTED
+        writer2.write(MergingTest.MESSAGE_3, follower2);
+        Assertions.assertThat(merge.getMessages()).hasSize(1); // one ACCEPTED
+        writer1.write(MergingTest.MESSAGE_4, follower1);
+        // third follows the first file; merge should have the message just once
+        final Follower follower3 = watch1.startFollowing();
+        final MergingFollower merge2 = follower3.mergeWith(merge);
+        Assertions.assertThat(merge.getMerged()).containsOnly(follower1, follower2);
+        Assertions.assertThat(merge2).isNotSameAs(merge);
+        Assertions.assertThat(merge2.getMerged()).containsOnly(follower1, follower2, follower3);
+        writer1.write(MergingTest.MESSAGE_5, follower3);
+        Assertions.assertThat(merge.getMessages()).hasSize(3);
+        Assertions.assertThat(merge2.getMessages()).hasSize(3);
+        // test writing
+        final List<String> messages = new LinkedList<String>();
+        messages.add(MergingTest.MESSAGE_1);
+        messages.add(MergingTest.MESSAGE_2);
+        messages.add(MergingTest.MESSAGE_4);
+        try {
+            final File f = DefaultFollowerBaseTest.getTempFile();
+            merge2.write(new FileOutputStream(f), NoopMessageFormatter.INSTANCE);
+            Assertions.assertThat(f).exists();
+            final List<String> lines = FileUtils.readLines(f, "UTF-8");
+            Assertions.assertThat(lines).isEqualTo(messages);
+        } catch (final Exception e) {
+            Assertions.fail("Couldn't write to file.");
+        }
+        // and now separate the second, making MESSAGE_2 disappear
+        Assertions.assertThat(merge2.separate(follower2)).isTrue();
+        Assertions.assertThat(merge2.getMessages()).hasSize(2);
+        // now only MESSAGE_4 remains ACCEPTED
+        Assertions.assertThat(merge2.separate(follower1)).isTrue();
+        Assertions.assertThat(merge2.getMessages()).hasSize(1);
+        // and now stop the followers to see what happens to the merges
+        follower3.stop();
+        Assertions.assertThat(merge2.isStopped()).isTrue();
+        Assertions.assertThat(merge.isStopped()).isFalse();
+        watch1.terminate();
+        Assertions.assertThat(follower1.isStopped());
+        Assertions.assertThat(merge2.isStopped()).isTrue();
+        Assertions.assertThat(merge.isStopped()).isFalse();
+        merge.stop();
+        Assertions.assertThat(merge.isStopped()).isTrue();
+        Assertions.assertThat(merge2.isStopped()).isTrue();
+        Assertions.assertThat(follower2.isStopped()).isTrue();
     }
 
     @Test
@@ -36,11 +106,11 @@ public class MergingTest extends DefaultFollowerBaseTest {
         Assertions.assertThat(mf.isStopped()).isFalse();
         Assertions.assertThat(this.getLogWatch().stopFollowing(f2)).isTrue();
         Assertions.assertThat(mf.isStopped()).isTrue(); // no followers are
-                                                           // following
+        // following
         Assertions.assertThat(mf.getMessages().size()).isEqualTo(2);
         mf.separate(f2);
         Assertions.assertThat(mf.isStopped()).isTrue(); // no followers are
-                                                           // following
+        // following
         Assertions.assertThat(mf.getMessages().size()).isEqualTo(0);
         // none of these changes should have affected the second merge
         Assertions.assertThat(mf2.getMessages().size()).isEqualTo(2);
@@ -51,5 +121,19 @@ public class MergingTest extends DefaultFollowerBaseTest {
         Assertions.assertThat(mf2.getMessages().size()).isEqualTo(1);
         this.getLogWatch().terminate();
         Assertions.assertThat(mf.isStopped()).isTrue();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMergeWithSelf() {
+        final Follower f = this.getLogWatch().startFollowing();
+        f.mergeWith(f);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMergeWithSelf2() {
+        final Follower f = this.getLogWatch().startFollowing();
+        final Follower f2 = this.getLogWatch().startFollowing();
+        final MergingFollower mf = f.mergeWith(f2);
+        mf.mergeWith(mf);
     }
 }
