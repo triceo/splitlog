58759d16f9e5e0dbb5475dffc4f840a43cdd3f93
Lukáš Petrovický <lpetrovi@redhat.com>
2014-05-15 19:27:45 -0400

    Enforce test timeout on a Surefire level

--- splitlog-core/src/test/java/com/github/triceo/splitlog/AbstractSplitlogTest.java
+++ splitlog-core/src/test/java/com/github/triceo/splitlog/AbstractSplitlogTest.java
@@ -1,25 +1,19 @@
 package com.github.triceo.splitlog;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Rule;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
-import org.junit.rules.Timeout;
 import org.junit.runner.Description;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Base class for all Splitlog tests. Will enforce a default timeout and add
- * some useful test execution logging.
+ * Base class for all Splitlog tests. Will enforce some useful test execution
+ * logging.
  */
 public abstract class AbstractSplitlogTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractSplitlogTest.class);
-
-    @Rule
-    public Timeout globalTimeout = new Timeout((int) TimeUnit.MINUTES.toMillis(1));
 
     @Rule
     public TestRule watchman = new TestWatcher() {
